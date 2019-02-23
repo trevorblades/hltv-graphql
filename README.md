21e8beedd734d616ec8036292ec3d84ab1c8bde2
Trevor Blades <tdblades@gmail.com>
2019-02-22 23:37:05 -0800

    Remove express

--- README.md
+++ README.md
@@ -11,11 +11,11 @@ Clone this repo and `cd` into it. You're going to need to create a `.env` file t
 PORT=4000
 ```
 
-Next, install the project dependencies and start up the dev server. If everything works, you'll see a 🚀 successful 🚀 message in your terminal.
+Next, install the project dependencies and start up the dev server. If everything works, you'll see a message in your terminal with the URL that the API is available at.
 
 ```bash
 $ npm install
 $ npm start
 ```
 
-If you access the provided URL in a browser ([http://localhost:4000/graphql](http://localhost:4000/graphql) in this example), you'll get a GraphQL playground to read over the API schema and test out some queries.
+If you access the provided URL in a browser ([http://localhost:4000](http://localhost:4000) in this example), you'll get a GraphQL playground to read over the API schema and test out some queries.
--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-import express from 'express';
-import {ApolloServer} from 'apollo-server-express';
+import {ApolloServer} from 'apollo-server';
 import {resolvers, typeDefs} from './schema';
-
-const app = express();
-
-// app.get('/', async (req, res) => {
-//   try {
-//     const rankings = await hltv.getTeamRanking();
-//     const teams = await Promise.all(
-//       rankings.slice(0, 5).map(ranking => hltv.getTeam({id: ranking.team.id}))
-//     );
-
-//     const players = await Promise.all(
-//       teams.flatMap(team =>
-//         team.players.map(player => hltv.getPlayer({id: player.id}))
-//       )
-//     );
-
-//     res.send(players);
-//   } catch (error) {
-//     res.status(500).send(error);
-//   }
-// });
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-server.applyMiddleware({app});
-
-app.listen(process.env.PORT, () => {
-  console.log(
-    `🚀 Server ready at http://localhost:${process.env.PORT}${
-      server.graphqlPath
-    }`
-  );
+server.listen(process.env.PORT).then(({url}) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
