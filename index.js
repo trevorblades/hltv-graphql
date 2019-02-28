b36240a482d446a3d922cf2d05cd4efa91840bfa
Trevor Blades <tdblades@gmail.com>
2019-02-27 22:12:53 -0800

    Guard api with keyholder

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-import {ApolloServer} from 'apollo-server';
+import Keyholder from 'keyholder';
+import {ApolloServer, AuthenticationError} from 'apollo-server';
 import {resolvers, typeDefs} from './schema';
+
+const keyholder = new Keyholder({
+  id: process.env.KEYHOLDER_ID,
+  apiKey: process.env.KEYHOLDER_API_KEY
+});
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  async context({req}) {
+    const isValid = await keyholder.test(req.query.apiKey);
+    if (!isValid) {
+      throw new AuthenticationError('Invalid API key');
+    }
+
+    return {
+      authenticated: true
+    };
+  }
 });
 
 server.listen(process.env.PORT).then(({url}) => {
